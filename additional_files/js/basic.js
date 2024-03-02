becf74007f189e0068d89c52912cb6b5331b8328
Senex-Nostalgia <zhafde120712@outlook.com>
2024-03-03 04:04:05 +0800

    JS file fixed

--- additional_files/js/basic.js
+++ additional_files/js/basic.js
@@ -19,7 +19,7 @@ const
 		atcoder_flag = getStorage('atcoder-portal') === 'minor';
 
 	const
-		DisplayDict = { 'Luogu': '洛谷', 'SPOJ': 'Sphere OJ', 'rg': 'Regulus OJ', 'Local': '本地', 'NowCoder': '牛客', 'zr': '正睿', 'Unknown': '其他的 OJ', 'HaoBa': 'HJJ'},
+		DisplayDict = { 'Luogu': '洛谷', 'SPOJ': 'Sphere OJ', 'rg': 'Regulus OJ', 'Local': '本地', 'NowCoder': '牛客', 'zr': '正睿', 'Unknown': '用得比较少的OJ', 'HaoBa': 'HaoBa'},
 		NormDict = { 'bzoj': 'BZOJ', 'lg': 'Luogu', 'uoj': 'UOJ', 'loj': 'LibreOJ', 'cf': 'Codeforces', 'gym': 'Codeforces', 'cc': 'Codechef', 'ac': 'AtCoder', 'agc': 'AtCoder', 'arc': 'AtCoder', 'abc': 'AtCoder', 'spoj': 'SPOJ', 'qoj': 'QOJ', 'tc': 'TopCoder', 'ykc' : "YukiCoder", 'nc': 'NowCoder', 'hb' : 'HaoBa', 'mx': 'MengXiong'},
 		SiteDict = {
 			'bzoj': [/^(\d+)$/, x => `https://darkbzoj.cc/problem/${x}`],
